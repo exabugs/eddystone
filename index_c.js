d78d62c898d13e7ffb93f382a641b1c53e5e99b8
exabugs <exabugs@gmail.com>
2017-07-20 14:17:34 +0900

    Add C

---
+++ index_c.js
@@ -0,0 +1,6 @@
+// https://exabugs.github.io/eddystone/index_roomC.html
+var url = 'https://goo.gl/4onXHH'
+
+var eddystoneBeacon = require('eddystone-beacon');
+eddystoneBeacon.advertiseUrl(url);
+
