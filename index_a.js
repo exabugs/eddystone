af08aec87e288228bed347e2cf93387568583cd1
exabugs <exabugs@gmail.com>
2017-07-20 13:08:46 +0900

    Fix index_X.js

--- index_a.js
+++ index_a.js
@@ -1,17 +1,6 @@
-//var url = 'https://recochoku.jp/';
-//var url = 'https://www.dreamarts.co.jp/';
-
-//var url = 'http://exabugs-www.s3-website-ap-northeast-1.amazonaws.com/index_roomA.html'
-var url = 'https://goo.gl/auzWgy'
-
-// GitHub Pages webIDE
-var url = 'https://goo.gl/pj1CgR'
+// https://exabugs.github.io/eddystone/index_roomA.html
+var url = 'https://goo.gl/ZDP1sM'
 
 var eddystoneBeacon = require('eddystone-beacon');
-
-//var namespaceId = '00010203040506070809';
-//var instanceId = 'aabbccddeeff';
-//eddystoneBeacon.advertiseUid(namespaceId, instanceId);
-
 eddystoneBeacon.advertiseUrl(url);
 
--- index_b.js
+++ index_b.js
@@ -1,8 +1,5 @@
-//var url = 'https://recochoku.jp/';
-//var url = 'https://www.dreamarts.co.jp/';
-
-var url = 'http://exabugs-www.s3-website-ap-northeast-1.amazonaws.com/index_roomB.html'
-var url = 'https://goo.gl/rRkH3T'
+// https://exabugs.github.io/eddystone/index_roomB.html
+var url = 'https://goo.gl/yhYnRm'
 
 var eddystoneBeacon = require('eddystone-beacon');
 eddystoneBeacon.advertiseUrl(url);
